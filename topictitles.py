bd26b3ead817d18cddfce665072e194f4af580cc
Joseph Widjaja <jewidjaj@ucsd.edu>
2016-09-18 01:45:14 -0700

    need to change list to dictionary

--- topictitles.py
+++ topictitles.py
@@ -2,19 +2,42 @@
 
 from bs4 import BeautifulSoup
 import requests
+import threading
 
-def main():
-    url = raw_input("Enter a subreddit to see new topics: ")
+url = raw_input("Enter a subreddit: ")
+key = raw_input("Enter substring: ")
 
+#titleList = {}
+
+def getTitles():
     r = requests.get("http://reddit.com/r/" + url + "/new/")
-
-    #print(url)
 
     data = r.text
 
     soup = BeautifulSoup(data, "html.parser")
 
-    for title in soup.find_all('p', class_='title'):
-        print(title.find_all(text=True))[0]
+    titleList = []
 
-main()
+    for title in soup.find_all('p', class_='title'):
+        #print(title.find_all(text=True))[0]
+        pair = ((title.find_all(text=True))[0], False)
+        titleList.append(pair)
+        #titleList[(title.find_all(text=True))[0]] = False
+
+    for place, temp in enumerate(titleList):
+        (hit, read) = temp
+        #print(str(hit))
+        #print(read)
+        if str(key) not in str(hit) or read is True:
+            continue
+        setTrue = (hit, True)
+        titleList[place] = setTrue
+        print(str(hit))
+        #print(titleList[place][1])
+
+
+    print("\n")
+
+    threading.Timer(15, getTitles).start()
+
+getTitles()
